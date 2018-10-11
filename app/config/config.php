de8dde36f8f1e358896df774503189b72d207024
Gutz <gutzby.marzan@digify.com.ph>
2018-10-11 19:23:28 +0800

    Change in config file

--- app/config/config.php
+++ app/config/config.php
@@ -10,7 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-define('PREFIX', 'ortigascms_');
+define('PREFIX', 'tgobeyondtheclouds_');
 
 /*
 |--------------------------------------------------------------------------
@@ -45,7 +45,7 @@ $config['cache_drivers'] = array('adapter' => 'redis', 'backup' => 'file', 'key_
 
 // NOTE: Do not change this unless it's a new project. 
 //       Please configure your web server to use this path
-$config['base_url'] = 'http://ortigas.cms.local';
+$config['base_url'] = 'http://oclpcms.tgobeyondtheclouds.com/';
 
 /*
 |--------------------------------------------------------------------------
