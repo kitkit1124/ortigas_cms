d84e28d942312e77bd24211d952d86216d363710
Gutz <gutzby.marzan@digify.com.ph>
2018-10-15 11:25:20 +0800

    config

--- app/config/config.php
+++ app/config/config.php
@@ -10,7 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-define('PREFIX', 'ortigascms_');
+define('PREFIX', 'oclpcms_');
 
 /*
 |--------------------------------------------------------------------------
