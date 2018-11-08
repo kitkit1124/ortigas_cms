0db7c1a705fe27865e095220b31b1630bc7f168d
Gutz <gutzby.marzan@digify.com.ph>
2018-11-08 18:06:05 +0800

    update npm

---
+++ pub/npm/tinymce/plugins/fontawesome/index.js
@@ -0,0 +1,7 @@
+// Exports the "fontawesome" plugin for usage with module loaders
+// Usage:
+//   CommonJS:
+//     require('tinymce/plugins/fontawesome')
+//   ES2015:
+//     import 'tinymce/plugins/fontawesome'
+require('./plugin.js');
