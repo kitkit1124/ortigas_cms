8441606360e6fa113498194e7b03b5508d93665d
Gutz <gutzby.marzan@digify.com.ph>
2018-12-17 17:21:58 +0800

    photo update

--- app/modules/website/migrations/018_create_seo.php
+++ app/modules/website/migrations/018_create_seo.php
@@ -73,50 +73,9 @@ class Migration_Create_seo extends CI_Migration {
 
 
 		$data = array(
-			array(
-				'seo_title'  => 'Google Tag Manager', 
-				'seo_content' => 	"<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WHHR9T8');</script>
-<!-- End Google Tag Manager -->",
-				'seo_status' => 'Active',
-			),
-			array(
-				'seo_title'  => 'Facebook Pixel Code', 
-				'seo_content' =>	"<!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0; t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '365076777159631', { em: 'insert_email_variable,'});
-fbq('track', 'PageView');
-</script>
-<noscript><img height='1' width='1' style='display:none' src='https://www.facebook.com/tr?id=365076777159631&ev=PageView&noscript=1'/></noscript>
-<!-- DO NOT MODIFY -->
-<!-- End Facebook Pixel Code -->",
-				'seo_status' => 'Active',
-			),
-			array(
-				'seo_title'  => 'Google Code for Remarketing Tag', 
-				'seo_content' =>   '<!-- Google Code for Remarketing Tag -->
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = 867370377;
-var google_custom_params = window.google_tag_params;
-var google_remarketing_only = true;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
-<noscript><div style="display:inline;"><img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/867370377/?guid=ON&amp;script=0"/></div>
-<!-- End Google Code for Remarketing Tag -->
-</noscript>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHHR9T8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->',
-				'seo_status' => 'Active',
-			),	
+			array('seo_title'  => 'Google Tag Manager', 'seo_content' => '', 'seo_status' => 'Active'),
+			array('seo_title'  => 'Facebook Pixel Code', 'seo_content' => '','seo_status' => 'Active'),
+			array('seo_title'  => 'Google Code for Remarketing Tag', 'seo_content' => '', 'seo_status' => 'Active'),	
 		);
 		$this->db->insert_batch($this->_table, $data);
 	}
