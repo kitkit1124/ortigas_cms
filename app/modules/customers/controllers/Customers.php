862b5dedfa9038ef920e9834dbb6b4c5207f8b2e
Jaime  Ramos <jaime.ramos@digify.com.ph>
2019-10-24 17:12:49 +0800

    Update Customers

--- app/modules/customers/controllers/Customers.php
+++ app/modules/customers/controllers/Customers.php
@@ -240,7 +240,6 @@ class Customers extends MX_Controller {
 	 */
 	Public function email_validation($email)
 	{
-	
 		$result = preg_match('/^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/',$email);
 		if($result)
 		{
